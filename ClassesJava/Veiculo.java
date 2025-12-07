b362bb76b9e20df17f30ed8c35dc6c40d8374ad8
LeonardoRibeiro-cpu <leonardo.ribeiro@estudante.iftm.edu.br>
2025-12-07 16:30:23 -0300

    Classe veiculo pronto

--- ClassesJava/Veiculo.java
+++ ClassesJava/Veiculo.java
@@ -1,4 +1,18 @@
 package ClassesJava;
 public class Veiculo {
-    
-}
+    private String placa;
+    private String modelo;
+    private String cor;
+
+    public Veiculo(){
+    }
+
+    public Veiculo(String placa, String modelo, String cor) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.cor = cor;
+    }
+   public String exibe() {
+    return modelo + "Cor: "+ cor + " - Placa: " + placa;
+ }
+} 
