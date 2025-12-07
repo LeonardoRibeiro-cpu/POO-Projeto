1ff9461465e205e4ad67e2f61ba9b808aa7dc08c
LeonardoRibeiro-cpu <leonardo.ribeiro@estudante.iftm.edu.br>
2025-12-07 16:49:31 -0300

    vagas pronto

--- ClassesJava/Vagas.java
+++ ClassesJava/Vagas.java
@@ -1,5 +1,41 @@
 package ClassesJava;
 
 public class Vagas {
-    
+    private int numero;
+    private boolean ocupada;
+    private Veiculo veiculo;
+    public Vagas() {
+    }
+       public Vagas(int numero) {
+        this.numero = numero;
+        this.ocupada = false;
+        this.veiculo = null;
+    }
+
+    public boolean ocupar(Veiculo v) {
+        if (this.ocupada) {
+            return false; 
+        }
+        this.veiculo = v;
+        this.ocupada = true;
+        return true;
+    }
+
+  
+    public boolean liberar() {
+        if (!this.ocupada) {
+            return false; 
+        }
+        this.veiculo = null;
+        this.ocupada = false;
+        return true;
+    }
+
+    public String exibe() {
+        if (ocupada) {
+            return "Vaga " + numero + " - Ocupada - " + veiculo.exibe();
+        } else {
+            return "Vaga " + numero + " - Livre";
+        }
+    }
 }
